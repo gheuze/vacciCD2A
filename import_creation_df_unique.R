0fd585a1c2185bf18c2ecdfce17e1345b88a2889
gheuze <gheuze@gmail.com>
2017-08-04 16:06:26 +0200

    import_creation_df_unique.R == rectification ddn mini a 1993 au lieu de 1995

--- import_creation_df_unique.R
+++ import_creation_df_unique.R
@@ -33,7 +33,7 @@ rm(donnees1,donnees2)
 donnees <- donnees[donnees$dateopv <= as.Date("2015-12-31"),]
 
 # restriction aux personnes nees apres le 1er janvier 1995
-donnees <- donnees[donnees$datenaiss > as.Date("1995-01-01"),]
+donnees <- donnees[donnees$datenaiss > as.Date("1993-01-01"),]
 
 # liste des vaccins ne nous interesssant pas, on enleve pour alleger la base
 a_exclure <- c("BCG","ENC JAP","FJ","G","L","RA","SHA","SHB","TAB","HPV",
