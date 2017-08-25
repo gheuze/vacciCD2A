2af86a7ec4a3407220e558eda4173786e977f9de
gheuze <gheuze@gmail.com>
2017-08-25 14:53:38 +0200

    COQ_courbe.R == modification légende

--- COQ_courbe.R
+++ COQ_courbe.R
@@ -42,11 +42,11 @@ rownames(graphe) <- c("primovaccination à 24 mois", "rappel à 24 mois","primovac
 couleurs <- c("blue")
 
 png("sorties/coq/evolution_CV_24-4.png",width=25 ,height=15, units="cm",res = 400)
-      matplot(t(graphe),type="b",pch = c(3,4,2,6), xlab = "année de naissance de l'enfant", ylim = c(45,100),ylab = "taux de couverture vaccinal en %",
+      matplot(t(graphe),type="b",pch = c(3,4,2,6), xlab = "année de naissance de l'enfant", ylim = c(75,100),ylab = "taux de couverture vaccinal en %",
                main = "Evolution de la couverture vaccinale coqueluche à l'âge de 24 mois et 4 ans,
-              pour les enfants nés entre 1993 et 2011 - axe des ordonnées débutant à 45 %", xaxt="n", col = couleurs, lty = 3)
+              pour les enfants nés entre 1993 et 2011 - axe des ordonnées débutant à 75 %", xaxt="n", col = couleurs, lty = 3)
       axis(1,at=1:19,labels=1993:2011)
-      legend(15,57,legend = rownames(graphe), pch = c(3,4,2,6),col = couleurs, cex=0.8,bty="n",lwd=1,lty = 3)
-      legend(15,48,legend = "objectif de vaccination", pch="",col = "darkorange", cex=0.8,bty="n",lwd=1,lty = 2)
+      legend(15,80,legend = rownames(graphe), pch = c(3,4,2,6),col = couleurs, cex=0.8,bty="n",lwd=1,lty = 3)
+      legend(15,76,legend = "objectif de vaccination", pch="",col = "darkorange", cex=0.8,bty="n",lwd=1,lty = 2)
       abline(h = 95, col = "darkorange",lty = 2)
 dev.off()
