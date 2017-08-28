44a2ed8194845b0517dda59e6d5257a58d0d4633
gheuze <gheuze@gmail.com>
2017-08-28 13:53:29 +0200

    MEN_courbe.R == changement légendes

--- MEN_courbe.R
+++ MEN_courbe.R
@@ -9,9 +9,9 @@ couleurs <- c("red","green","blue")
 png("sorties/men/evolution_CV_nat-reg-PMI.png",width=25 ,height=15, units="cm",res = 400)
       matplot(t(graphe),type="b",pch = c(0,0,3),xlab = "années", ylim = c(0,100),ylab = "taux de couverture vaccinal en %",
                main = "Comparaison de l'évolution de la couverture vaccinale méningocoque C
-      à l'âge de 24 mois suivant les sources, 2010-2015", xaxt="n",lty = c(1,1,3),col = couleurs)
+      à l'âge de 24 mois suivant les sources, 2010-2015", xaxt="n",lty = c(1,3,3),col = couleurs)
       axis(1,at=1:7,labels=2010:2016)
-      legend(5.5,20,legend = rownames(graphe), pch=c(0,0,3),col = couleurs, cex=0.8,bty="n",lwd=1,lty = c(1,1,3))
+      legend(5.5,20,legend = rownames(graphe), pch=c(0,0,3),col = couleurs, cex=0.8,bty="n",lwd=1,lty = c(1,3,3))
       legend(5.5,7,legend = "objectif de vaccination", pch="",col = "darkorange", cex=0.8,bty="n",lwd=1,lty = 2)
       abline(h = 95, col = "darkorange",lty = 2)
 dev.off()
